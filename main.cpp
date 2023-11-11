7a519964af8220d6fc1cdb622a7b8414e5cf00ad
scybd <874630058@qq.com>
2023-11-11 11:29:35 +0800

    ...

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <time.h>
 #include "NumDetector.h"
 #include "NumCal.h"
 using namespace cv;
@@ -14,20 +15,29 @@ int main()
 	templateNumDetector->allInOne();
 	// vector<Mat> sub_t_size = templateNumDetector->allInOne();
 
-	VideoCapture video("/home/thy/Documents/SRP_GuideTheRoboticArm/2222.avi");
-	// 定义输出视频流 
-    // VideoWriter out("out.mp4", VideoWriter::fourcc('X', '2', '6', '4'), 30, Size(640, 480)); 
-
 	// 相机图片数字检测类的实例化
 	FrameNumDetector* frameNumDetector = new FrameNumDetector();
 
-	//　数字匹配计算类的实例化
+	// 数字匹配计算类的实例化
 	NumCal numcal;
-	
+
+	// 图片来源
+	VideoCapture cap(0);
+	cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+    cap.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
+	// VideoCapture video("/home/thy/Documents/SRP_GuideTheRoboticArm/2222.avi");
+	// 定义输出视频流 
+	// VideoWriter out("out.mp4", VideoWriter::fourcc('X', '2', '6', '4'), 30, Size(640, 480));
+
+	// 计时变量
+	clock_t start,end;
 	while(1)
 	{
+		start = clock();
+
 		Mat frame;
-		bool isRead = video.read(frame);
+		bool isRead = cap.read(frame);
 		if(!isRead) {break;}
 
 		Mat show = frame.clone();
@@ -42,16 +52,21 @@ int main()
 		Mat result = numcal.numberRecognition(*templateNumDetector, *frameNumDetector, show, 400);
 		result = numcal.calPos(result);
 
+		end = clock();
+		short funTime = (end-start)*1000/CLOCKS_PER_SEC;
+		putText(result, to_string(funTime)+"ms", Point(500, 440), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 0), 2);
+		
+
 		// out.write(result); 		// 写入一帧到输出视频文件中
 		imshow("res", result);
 
-		if(waitKey(30) == 'q') {break;}
+		if(waitKey(1) == 'q') {break;}
 
 	}
 	
 	delete frameNumDetector;
 	delete templateNumDetector;
 	destroyAllWindows();
-	video.release();
+	cap.release();
 	return 0;
 }
